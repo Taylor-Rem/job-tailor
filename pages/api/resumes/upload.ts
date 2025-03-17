4205c589bd78f826bdf9e79c823aac29c66f1a27
Taylor Remund <taylorremund@Taylors-MacBook-Pro.local>
2025-03-17 12:03:27 -0600

    replaced env variables to not use AWS in name

--- pages/api/resumes/upload.ts
+++ pages/api/resumes/upload.ts
@@ -3,10 +3,10 @@ import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { Pool } from 'pg';
 
 const s3 = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.ACCESS_KEY_ID!,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY!,
   },
 });
 
